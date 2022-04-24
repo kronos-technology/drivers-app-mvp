67cc83ceafb2a8b20b057f4b9a89236d9bd9bce6
Anderson <andersong.9012@gmail.com>
2022-04-23 19:16:05 -0500

    Add inital config with yarn

--- lib/drivers-app-mvp-stack.ts
+++ lib/drivers-app-mvp-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cognito from '@aws-cdk/aws-cognito'
 
 export class DriversAppMvpStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {

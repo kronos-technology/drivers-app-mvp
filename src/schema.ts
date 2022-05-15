422b6157556928c8dd21dbc4fca40730a2e74724
Anderson <andersong.9012@gmail.com>
2022-05-15 12:15:15 -0500

    Add id to some entities

--- src/schema.ts
+++ src/schema.ts
@@ -81,6 +81,7 @@ const Schema = {
     RouteInCompany: {
       pk: { type: String, value: 'company#${companyId}' },
       sk: { type: String, value: 'route#${routeId}' },
+      id: { type: String, generate: 'ulid', validate: Match.ulid },
       companyId: { type: String, required: true },
       routeId: { type: String, required: true },
       // To search all companies assigned to a route,
@@ -106,6 +107,7 @@ const Schema = {
     RouteInCheckpoint: {
       pk: { type: String, value: 'checkpoint#${checkpointId}' },
       sk: { type: String, value: 'route#${routeId}' },
+      id: { type: String, generate: 'ulid', validate: Match.ulid },
       checkpointId: { type: String, required: true },
       routeId: { type: String, required: true },
       destination: { type: String, required: true },

7ebb9e501d15b489059165de3a29168b35fbbac7
Anderson <andersong.9012@gmail.com>
2022-05-19 22:01:28 -0500

    Change models name to uppercase

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -20,7 +20,7 @@ const Schema = {
     gs2: { hash: 'gs2pk', sort: 'gs2sk' }
   },
   models: {
-    Driver: {
+    DRIVER: {
       pk: { type: String, value: 'driver#${driverId}' },
       sk: { type: String, value: 'driver' },
       id: { type: String, generate: 'ulid', validate: Match.ulid },
@@ -34,7 +34,7 @@ const Schema = {
       gs1pk: { type: String, value: 'driver#' },
       gs1sk: { type: String, value: 'driver#${companyId}' }
     },
-    Vehicle: {
+    VEHICLE: {
       pk: { type: String, value: 'vehicle#${plate}' },
       sk: { type: String, value: 'vehicle#' },
       id: { type: String, generate: 'ulid', validate: Match.ulid },
@@ -48,7 +48,7 @@ const Schema = {
       gs1pk: { type: String, value: 'vehicle#' },
       gs1sk: { type: String, value: 'vehicle#${companyId}' }
     },
-    Company: {
+    COMPANY: {
       pk: { type: String, value: 'company#${companyId}' },
       sk: { type: String, value: 'company#' },
       id: { type: String, generate: 'ulid', validate: Match.ulid },
@@ -61,7 +61,7 @@ const Schema = {
       gs1pk: { type: String, value: 'company#' },
       gs1sk: { type: String, value: 'company#${city}' }
     },
-    Route: {
+    ROUTE: {
       pk: { type: String, value: 'route#${id}' },
       sk: { type: String, value: 'route#' },
       id: { type: String, generate: 'ulid', validate: Match.ulid },
@@ -78,7 +78,7 @@ const Schema = {
     // RouteInCompany represents a route assigned
     // to a company. It helps to model the many-to-many
     // relation between routes and companies
-    RouteInCompany: {
+    ROUTEINCOMPANY: {
       pk: { type: String, value: 'company#${companyId}' },
       sk: { type: String, value: 'route#${routeId}' },
       id: { type: String, generate: 'ulid', validate: Match.ulid },
@@ -88,7 +88,7 @@ const Schema = {
       gs1pk: { type: String, value: 'route#${routeId}' },
       gs1sk: { type: String, value: 'route#company' }
     },
-    Checkpoint: {
+    CHECKPOINT: {
       pk: { type: String, value: 'checkpoint#${checkpointId}' },
       sk: { type: String, value: 'checkpoint#' },
       id: { type: String, generate: 'ulid', validate: Match.ulid },
@@ -104,7 +104,7 @@ const Schema = {
     // CheckpointInRoute represents a checkpoint contained
     // in a route. It helps to model the many-to-many
     // relation between checkpoints and routes
-    RouteInCheckpoint: {
+    ROUTEINCHECKPOINT: {
       pk: { type: String, value: 'checkpoint#${checkpointId}' },
       sk: { type: String, value: 'route#${routeId}' },
       id: { type: String, generate: 'ulid', validate: Match.ulid },
@@ -115,7 +115,7 @@ const Schema = {
       gs1pk: { type: String, value: 'route#${routeId}' },
       gs1sk: { type: String, value: 'route#checkpoint' }
     },
-    Checkin: {
+    CHECKIN: {
       pk: { type: String, value: 'checkpoint#${checkpointId}' },
       sk: { type: String, value: '${timestamp}#${plate}' },
       id: { type: String, generate: 'ulid', validate: Match.ulid },

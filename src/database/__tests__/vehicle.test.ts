b65455a63279aa3d6929969ef36aa03229bb8277
Anderson <andersong.9012@gmail.com>
2022-05-20 19:40:32 -0500

    Refactor code and tests for vehicle entity

--- src/database/__tests__/vehicle.test.ts
+++ src/database/__tests__/vehicle.test.ts
@@ -1,75 +1,97 @@
 // requireActual ensures you get the real file
 // instead of an automock
 // we use import type and <typeof ...> to still get types
-import * as vehicleLib from '../vehicle';
+import { Vehicle } from '../entities';
+import { EntityModel, VehicleType, Response } from '../base';
 
-test('Create vehicle', async () => {
-  const data: Array<vehicleLib.Vehicle> = [
-    {
-      plate: 'ZIY246',
-      status: 'idle',
-      number: '048',
-      companyId: 'SABANA',
-      currentDriverId: '1070959307'
-    },
-    {
-      plate: 'BAM577',
-      status: 'covering#fca-bog-cll13-pte-arnd#33%',
-      number: '048',
-      companyId: 'SABANA',
-      currentDriverId: '1070947951'
-    },
-    {
-      plate: 'ABC123',
-      status: 'covering#fca-bog-cll13-pte-arnd#55%',
-      number: '013',
-      companyId: 'AYACUCHO',
-      currentDriverId: '19134629'
-    }
-  ];
-  for (const item of data) {
-    const response = await vehicleLib.create(item);
-    console.log(response);
-    expect(response).toBeDefined();
+const vehicleTestData: Array<VehicleType> = [
+  {
+    plate: 'ZIY246',
+    status: 'idle',
+    number: '048',
+    companyId: 'SABANA',
+    currentDriverId: '1070959307'
+  },
+  {
+    plate: 'BAM577',
+    status: 'covering#fca-bog-cll13-pte-arnd#33%',
+    number: '048',
+    companyId: 'SABANA',
+    currentDriverId: '1070947951'
+  },
+  {
+    plate: 'ABC123',
+    status: 'covering#fca-bog-cll13-pte-arnd#55%',
+    number: '013',
+    companyId: 'AYACUCHO',
+    currentDriverId: '19134629'
   }
-  await new Promise((r) => setTimeout(r, 2000));
-});
+];
 
-test('List all vehicles', async () => {
-  const response = await vehicleLib.list();
-  console.log(response);
-  expect(response).toBeInstanceOf(Array);
-});
+describe.only('Vehicle Entity Tests', () => {
+  let vehicleManager: Vehicle;
+  let testVehicle: VehicleType;
+  let vehicleList: Array<EntityModel> | undefined;
 
-test('List vehicles by company', async () => {
-  const id = 'AYACUCHO';
-  const response = await vehicleLib.listByCompany(id);
-  console.log(response);
-  expect(response).toBeDefined();
-});
+  beforeAll(async () => {
+    vehicleManager = new Vehicle();
+  });
 
-test('Get vehicle by plate', async () => {
-  const testId = 'ZIY246';
-  const response = await vehicleLib.get(testId);
-  expect(response).toBeDefined();
-});
+  test.skip('create test vehicles', async () => {
+    let createdVehicles: Array<EntityModel | Response> = [];
+    for (const vehicleInfo of vehicleTestData) {
+      const created = await vehicleManager.create(vehicleInfo);
+      createdVehicles.push(created);
+      console.log(`Vehicle created ${JSON.stringify(created, null, 2)}`);
+      expect(created).toMatchObject(vehicleInfo);
+    }
+    expect(createdVehicles).toHaveLength(vehicleTestData.length);
+    await new Promise((r) => setTimeout(r, 2000));
+  });
 
-test('Update vehicle status', async () => {
-  const plate = 'ZIY246';
-  const newStatus = 'cover#bgt-vllta#22%';
-  const updateData = { plate: plate, status: newStatus };
-  const response = await vehicleLib.update(updateData);
-  console.log(response);
-  expect(response).toBeDefined();
-});
+  test('list all vehicles', async () => {
+    const list = await vehicleManager.list();
+    console.log(`List of all vehicles: ${JSON.stringify(list, null, 2)}`);
+    expect(list).toBeInstanceOf(Array);
+    expect(list).toHaveLength(vehicleTestData.length);
+  });
 
-test('Delete vehicle', async () => {
-  const id = 'ZIY246';
-  const vehicle = await vehicleLib.get(id);
-  const removed = await vehicleLib.remove(id);
-  console.log('Vehicle removed: ', removed);
-  expect(removed).toEqual(vehicle);
+  test('list vehicles by company', async () => {
+    const id = 'SABANA';
+    const list = await vehicleManager.listByCompany(id);
+    console.log(
+      `List of all vehicles for company ${id}: ${JSON.stringify(list, null, 2)}`
+    );
+    expect(list).toBeInstanceOf(Array);
+    expect(list).toHaveLength(2);
+  });
+
+  test('get vehicle', async () => {
+    const plate: string = vehicleTestData[0].plate;
+    const vehicle: EntityModel | undefined = await vehicleManager.get(plate);
+    console.log(`Vehicle info ${JSON.stringify(vehicle, null, 2)}`);
+    expect(vehicle).toBeDefined();
+  });
+
+  test('update vehicle status', async () => {
+    const plate: string = vehicleTestData[0].plate;
+    const updateData = { plate: plate, status: 'COVERING#FACA-BOGOTA-13' };
+    const response = await vehicleManager.update(updateData);
+    console.log(`Vehicle updated ${JSON.stringify(response, null, 2)}`);
+    expect(response).toBeDefined();
+  });
+
+  test.only('delete vehicle', async () => {
+    let deletedVehicles: Array<EntityModel | Response | undefined> = [];
+    for (const vehicleInfo of vehicleTestData) {
+      const plate: string = vehicleInfo.plate;
+      const deleted = await vehicleManager.delete(plate);
+      deletedVehicles.push(deleted);
+      console.log(`Vehicle deleted ${JSON.stringify(deleted, null, 2)}`);
+      expect(deleted).toBeDefined();
+    }
+    expect(deletedVehicles).toHaveLength(vehicleTestData.length);
+    await new Promise((r) => setTimeout(r, 2000));
+  });
+  // required with this small example
 });
-// required with this small example
-// to make the isolatedModules config happy
-export {};
--- src/database/vehicle.ts
+++ src/database/vehicle.ts
@@ -1,93 +1,23 @@
-import table from './util';
+import BaseEntity from './base';
 
-export type Vehicle = {
-  plate: String;
-  status: String;
-  number: String;
-  companyId: String;
-  lastMaintenance?: String;
-  currentDriverId?: String;
-};
-export type VehicleUpdate = {
-  plate: String;
-  status?: String;
-  number?: String;
-  companyId?: String;
-  lastMaintenance?: String;
-  currentDriverId?: String;
-};
+class Vehicle extends BaseEntity {
+  constructor(tableName?: string) {
+    super({ tableName, modelName: 'VEHICLE', idField: 'plate' });
+  }
 
-const VehicleModel = table.getModel('Vehicle');
-
-async function create(vehicleInfo: Vehicle) {
-  console.log('Creating new vehicle in database');
-  try {
-    const newVehicle = await VehicleModel.create(vehicleInfo, {
-      exists: false
-    });
-    return newVehicle;
-  } catch (err) {
-    console.log('DynamoDB error: ', err);
-    return null;
+  async listByCompany(companyId) {
+    console.log(`Listing vehicles for company ${companyId.toLowerCase()}`);
+    try {
+      const listByCompany = await this.model.find(
+        { companyId },
+        { index: 'gs1' }
+      );
+      return listByCompany;
+    } catch (error) {
+      console.error('DynamoDB error: ', error);
+      return null;
+    }
   }
 }
 
-async function get(id: String) {
-  console.log('Retrieving vehicle with id: ', id);
-  try {
-    const vehicleInfo = await VehicleModel.get({ plate: id });
-    console.log(vehicleInfo);
-    return vehicleInfo;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function list() {
-  console.log('Listing vehicles in database');
-  try {
-    const vehiclesList = await VehicleModel.find({}, { index: 'gs1' });
-    return vehiclesList;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function listByCompany(id: String) {
-  console.log('Listing vehicles for company ', id);
-  try {
-    const vehiclesList = await VehicleModel.find(
-      { companyId: id },
-      { index: 'gs1' }
-    );
-    return vehiclesList;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function update(data: VehicleUpdate) {
-  console.log('Updating vehicle ', data.plate);
-  try {
-    const vehicle = await VehicleModel.update(data);
-    return vehicle;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function remove(id: String) {
-  console.log('Deleting vehicle ', id);
-  try {
-    const removed = await VehicleModel.remove({ plate: id });
-    return removed;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-export { list, listByCompany, get, create, update, remove };
+export { Vehicle };

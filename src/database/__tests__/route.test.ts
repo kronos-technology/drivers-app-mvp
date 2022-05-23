6cea0d051b7138d1d915fd37028cec605d46a555
Anderson <andersong.9012@gmail.com>
2022-05-23 17:05:37 -0500

    Include tests for route entity in db

--- src/database/__tests__/route.test.ts
+++ src/database/__tests__/route.test.ts
@@ -1,91 +1,113 @@
-import * as routeLib from '../route';
+import { Route } from '../entities';
+import { EntityModel, RouteType, Response } from '../base';
 
-test('Create route', async () => {
-  const data: Array<routeLib.Route> = [
-    {
-      origin: 'FACATATIVA',
-      destination: 'BOGOTA',
-      intermediates: ['MADRID', 'CALLE13'],
-      geojson: 's3://locationtoroute1.geojson'
-    },
-    {
-      origin: 'FACATATIVA',
-      destination: 'BOGOTA',
-      intermediates: ['VARIANTE', 'CALLE13'],
-      geojson: 's3://locationtoroute2.geojson'
-    },
-    {
-      origin: 'FACATATIVA',
-      destination: 'BOGOTA',
-      intermediates: ['MOSQUERA', 'FUNZA', 'CALLE80'],
-      geojson: 's3://locationtoroute2.geojson'
-    },
-    {
-      origin: 'MADRID',
-      destination: 'BOGOTA',
-      intermediates: ['CALLE13'],
-      geojson: 's3://locationtoroute1.geojson'
-    },
-    {
-      origin: 'MADRID',
-      destination: 'BOGOTA',
-      intermediates: ['MOSQUERA', 'FUNZA', 'CALLE80'],
-      geojson: 's3://locationtoroute1.geojson'
-    },
-    {
-      origin: 'FACATATIVA',
-      intermediates: ['CORSO'],
-      destination: 'BOJACA',
-      geojson: 's3://locationtoroute2.geojson'
-    },
-    {
-      origin: 'FACATATIVA',
-      destination: 'ZIPAQUIRA',
-      intermediates: ['MOSQUERA', 'FUNZA', 'COTA', 'CHIA'],
-      geojson: 's3://locationtoroute2.geojson'
-    }
-  ];
-  for (const item of data) {
-    const response = await routeLib.create(item);
-    expect(response).toBeDefined();
+const routeTestData: Array<RouteType> = [
+  {
+    routeId: 'FACATA-MADRID-BOGOTA-13',
+    origin: 'FACATATIVA',
+    destination: 'BOGOTA',
+    intermediates: ['CALLE13'],
+    geojson: 's3://locationtoroute1.geojson'
+  },
+  {
+    routeId: 'FACATA-VTEMAD-BOGOTA-13',
+    origin: 'FACATATIVA',
+    destination: 'BOGOTA',
+    intermediates: ['MADRID', 'CALLE13'],
+    geojson: 's3://locationtoroute2.geojson'
+  },
+  {
+    routeId: 'FACATA-BOGOTA-80',
+    origin: 'FACATATIVA',
+    destination: 'BOGOTA',
+    intermediates: ['VARIANTE', 'MOSQUERA', 'FUNZA', 'CALLE13'],
+    geojson: 's3://locationtoroute3.geojson'
+  },
+  {
+    routeId: 'FACATA-BOJACA',
+    origin: 'FACATATIVA',
+    destination: 'BOJACA',
+    intermediates: ['CORSO'],
+    geojson: 's3://locationtoroute4.geojson'
+  },
+  {
+    routeId: 'BOJACA-BOGOTA',
+    origin: 'BOJACA',
+    destination: 'BOGOTA',
+    intermediates: ['VTEMAD'],
+    geojson: 's3://locationtoroute5.geojson'
   }
-  await new Promise((r) => setTimeout(r, 2000));
-});
+];
 
-test('List routes', async () => {
-  const response = await routeLib.list();
-  console.log(response);
-  expect(response).toBeInstanceOf(Array);
-});
+describe.only('route tests', () => {
+  let routeManager: Route;
 
-test('List routes by destination', async () => {
-  const destination: String = 'Zipaquira';
-  const response = await routeLib.listByDestination(destination);
-  console.log(response);
-  expect(response).toBeInstanceOf(Array);
-});
+  beforeAll(async () => {
+    routeManager = new Route();
+  });
 
-test('List routes by origin ', async () => {
-  const origin: String = 'MADRID';
-  const response = await routeLib.listByOrigin(origin);
-  console.log(response);
-  expect(response).toBeInstanceOf(Array);
-});
+  test('create test routes', async () => {
+    let createdRoutes: Array<EntityModel | Response> = [];
+    for (const routeInfo of routeTestData) {
+      const created = await routeManager.create(routeInfo);
+      createdRoutes.push(created);
+      console.log(`route created ${JSON.stringify(created, null, 2)}`);
+      expect(created).toMatchObject(routeInfo);
+    }
+    expect(createdRoutes).toHaveLength(routeTestData.length);
+    await new Promise((r) => setTimeout(r, 2000));
+  });
 
-test('Get route', async () => {
-  const id = 'MADRID-BOGOTA-1';
-  const response = await routeLib.get(id);
-  console.log(response);
-  expect(response).toBeDefined();
-});
+  test('list all routes', async () => {
+    const list = await routeManager.list();
+    console.log(`List of all routes: ${JSON.stringify(list, null, 2)}`);
+    expect(list).toBeInstanceOf(Array);
+    expect(list).toHaveLength(routeTestData.length);
+  });
 
-test('Delete route', async () => {
-  const id = 'FACATA-BOGOTA-1';
-  const route = await routeLib.get(id);
-  const removed = await routeLib.remove(id);
-  console.log('route removed: ', removed);
-  expect(removed).toEqual(route);
+  test('list routes by origin', async () => {
+    const origin = 'BOJACA';
+    const list = await routeManager.listByOrigin(origin);
+    console.log(
+      `List of all routes with origin: ${origin.toLowerCase()}. ${JSON.stringify(
+        list,
+        null,
+        2
+      )}`
+    );
+    expect(list).toBeInstanceOf(Array);
+    expect(list).toHaveLength(1);
+  });
+
+  test('get route', async () => {
+    const routeId: string = routeTestData[0].routeId;
+    const route: EntityModel | undefined = await routeManager.get(routeId);
+    console.log(`route info ${JSON.stringify(route, null, 2)}`);
+    expect(route).toBeDefined();
+  });
+
+  test('update route geojson path', async () => {
+    const routeId: string = routeTestData[0].routeId;
+    const updateData = {
+      routeId: routeId,
+      geojson: 's3://newrouteinanotherbucket'
+    };
+    const response = await routeManager.update(updateData);
+    console.log(`route updated ${JSON.stringify(response, null, 2)}`);
+    expect(response).toBeDefined();
+  });
+
+  test('delete route', async () => {
+    let deletedroutes: Array<EntityModel | Response | undefined> = [];
+    for (const routeInfo of routeTestData) {
+      const routeId: string = routeInfo.routeId;
+      const deleted = await routeManager.delete(routeId);
+      deletedroutes.push(deleted);
+      console.log(`route deleted ${JSON.stringify(deleted, null, 2)}`);
+      expect(deleted).toBeDefined();
+    }
+    expect(deletedroutes).toHaveLength(routeTestData.length);
+    await new Promise((r) => setTimeout(r, 2000));
+  });
+  // required with this small example
 });
-// required with this small example
-// to make the isolatedModules config happy
-export {};
--- src/database/route.ts
+++ src/database/route.ts
@@ -1,125 +1,52 @@
-import table from './util';
+import BaseEntity, { EntityModel } from './base';
+import { Model } from 'dynamodb-onetable';
 
-export type Route = {
-  origin: String;
-  intermediates?: Array<String>;
-  destination: String;
-  geojson: String;
-};
+class Route extends BaseEntity {
+  routeInCheckpointModel: Model<EntityModel>;
+  constructor(tableName?: string) {
+    super({ tableName, modelName: 'ROUTE', idField: 'routeId' });
+    this.routeInCheckpointModel =
+      this.table.getModel<EntityModel>('ROUTEINCHECKPOINT');
+  }
 
-export type RouteUpdate = {
-  id: String;
-  intermediates?: Array<String>;
-  geojson: String;
-};
+  async getCheckpoints(routeId: string) {
+    console.log(`Retrieving checkpoints in route: ${routeId}`);
+    try {
+      const checkpoints = await this.routeInCheckpointModel.find(
+        { routeId },
+        { index: 'gs1' }
+      );
+      return checkpoints;
+    } catch (error) {
+      console.error('DynamoDB error: ', error);
+      return null;
+    }
+  }
 
-const RouteModel = table.getModel('Route');
-const RouteInCheckpointModel = table.getModel('RouteInCheckpoint');
+  async listByOrigin(origin: string) {
+    console.log(`Listing routes with origin ${origin}`);
+    try {
+      const routesList = await this.model.find({ origin }, { index: 'gs1' });
+      return routesList;
+    } catch (error) {
+      console.error('DynamoDB error: ', error);
+      return null;
+    }
+  }
 
-async function create(routeInfo: Route) {
-  console.log('Creating new route in database');
-  try {
-    const created = await RouteModel.create(routeInfo, { exists: false });
-    return created;
-  } catch (err) {
-    console.log('DynamoDB error: ', err);
-    return null;
+  async listByDestination(destination: string) {
+    console.log(`Listing routes with destination ${destination}`);
+    try {
+      const routesList = await this.model.find(
+        { destination },
+        { index: 'gs2' }
+      );
+      return routesList;
+    } catch (error) {
+      console.error('DynamoDB error: ', error);
+      return null;
+    }
   }
 }
 
-async function get(id: String) {
-  console.log('Retrieving route with id: ', id);
-  try {
-    const routeInfo = await RouteModel.get({ routeId: id });
-    console.log(routeInfo);
-    return routeInfo;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function getCheckpoints(routeId: String) {
-  console.log(`Retrieving checkpoints in route: ${routeId}`);
-  try {
-    const checkpoints = await RouteInCheckpointModel.find(
-      { routeId: routeId },
-      { index: 'gs1' }
-    );
-    console.log(checkpoints);
-    return checkpoints;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function list() {
-  console.log('Listing routes in database');
-  try {
-    const routesList = await RouteModel.find({}, { index: 'gs1' });
-    return routesList;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function listByOrigin(origin: String) {
-  console.log('Listing routes with origin ', origin);
-  try {
-    const routesList = await RouteModel.find(
-      { origin: origin },
-      { index: 'gs1' }
-    );
-    return routesList;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-async function listByDestination(destination: String) {
-  console.log('Listing routes with destination ', destination);
-  try {
-    const routesList = await RouteModel.find(
-      { destination: destination },
-      { index: 'gs2' }
-    );
-    return routesList;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-// TODO: Add unit test for this function
-async function update(data: RouteUpdate) {
-  console.log(`Updating route -> ${JSON.stringify(data)}`);
-  try {
-    const vehicle = await RouteModel.update(data);
-    return vehicle;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function remove(id: String) {
-  console.log('Deleting route ', id);
-  try {
-    const removed = await RouteModel.remove({ routeId: id });
-    return removed;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-export {
-  list,
-  listByOrigin,
-  listByDestination,
-  get,
-  getCheckpoints,
-  create,
-  update,
-  remove
-};
+export { Route };

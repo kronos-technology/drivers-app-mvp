61ba16fbf62f9ce5077015b05c0cd4561329ce69
Anderson <andersong.9012@gmail.com>
2022-05-23 18:58:26 -0500

    Add tests for driver entity

--- src/database/__tests__/driver.test.ts
+++ src/database/__tests__/driver.test.ts
@@ -1,78 +1,105 @@
 // requireActual ensures you get the real file
 // instead of an automock
 // we use import type and <typeof ...> to still get types
-import * as driverLib from '../driver';
+import { Driver } from '../entities';
+import { EntityModel, DriverType, Response } from '../base';
 
-test('Create driver', async () => {
-  const data: Array<driverLib.Driver> = [
-    {
-      driverId: '1070959307',
-      name: 'Walla',
-      lastName: 'Gonzalez',
-      companyId: 'SABANA',
-      birthdate: '1990-12-06',
-      phone: '+573103308156'
-    },
-    {
-      driverId: '1070947951',
-      name: 'Fredy',
-      lastName: 'Gonzalez',
-      companyId: 'AYACUCHO',
-      birthdate: '1987-09-08',
-      phone: '+573058134980'
-    },
-    {
-      driverId: '19134629',
-      name: 'Alfonso',
-      lastName: 'Gonzalez',
-      companyId: 'SABANA',
-      birthdate: '1991-01-28',
-      phone: '+573194106391'
-    }
-  ];
-  for (const item of data) {
-    const response = await driverLib.create(item);
-    expect(response).toBeDefined();
+const driverTestData: Array<DriverType> = [
+  {
+    driverId: '1070959307',
+    name: 'Walla',
+    lastName: 'Gonzalez',
+    companyId: 'SABANA',
+    birthdate: '1990-12-06',
+    phone: '+573103308156'
+  },
+  {
+    driverId: '1070947951',
+    name: 'Fredy',
+    lastName: 'Gonzalez',
+    companyId: 'AYACUCHO',
+    birthdate: '1987-09-08',
+    phone: '+573058134980'
+  },
+  {
+    driverId: '19134629',
+    name: 'Alfonso',
+    lastName: 'Gonzalez',
+    companyId: 'SABANA',
+    birthdate: '1991-01-28',
+    phone: '+573194106391'
   }
-  await new Promise((r) => setTimeout(r, 2000));
-});
+];
 
-test('List all drivers', async () => {
-  const response = await driverLib.list();
-  console.log(response);
-  expect(response).toBeInstanceOf(Array);
-});
+describe.only('driver tests', () => {
+  let driverManager: Driver;
 
-test('List drivers by company', async () => {
-  const id = 'SABANA';
-  const response = await driverLib.listByCompany(id);
-  console.log(response);
-  expect(response).toBeDefined();
-});
+  beforeAll(async () => {
+    driverManager = new Driver();
+  });
 
-test('Get driver by Id', async () => {
-  const testId = '1070959307';
-  const response = await driverLib.get(testId);
-  expect(response).toBeDefined();
-});
+  test.skip('create test drivers', async () => {
+    let createdDrivers: Array<EntityModel | Response> = [];
+    for (const driverInfo of driverTestData) {
+      const created = await driverManager.create(driverInfo);
+      createdDrivers.push(created);
+      console.log(`driver created ${JSON.stringify(created, null, 2)}`);
+      expect(created).toBeDefined();
+    }
+    expect(createdDrivers).toHaveLength(driverTestData.length);
+    await new Promise((r) => setTimeout(r, 2000));
+  });
 
-test('Update driver name', async () => {
-  const id = '1070959307';
-  const newName = 'Anderson';
-  const newLastName = 'Gonzalez Garcia';
-  const updateData = { driverId: id, name: newName, lastName: newLastName };
-  const response = await driverLib.update(updateData);
-  console.log(response);
-  expect(response).toBeDefined();
-});
+  test('list all drivers', async () => {
+    const list = await driverManager.list();
+    console.log(`List of all drivers: ${JSON.stringify(list, null, 2)}`);
+    expect(list).toBeInstanceOf(Array);
+    expect(list).toHaveLength(driverTestData.length);
+  });
 
-test('Delete driver', async () => {
-  const id = '19134629 ';
-  const driver = await driverLib.get(id);
-  const removed = await driverLib.remove(id);
-  console.log('Driver removed: ', removed);
-  expect(removed).toEqual(driver);
+  test('list drivers by company', async () => {
+    const companyId = 'SABANA';
+    const list = await driverManager.listByCompany(companyId);
+    console.log(
+      `List of all drivers with origin: ${companyId}. ${JSON.stringify(
+        list,
+        null,
+        2
+      )}`
+    );
+    expect(list).toBeInstanceOf(Array);
+    expect(list).toHaveLength(2);
+  });
+
+  test('get driver', async () => {
+    const driverId: string = driverTestData[0].driverId;
+    const driver: EntityModel | undefined = await driverManager.get(driverId);
+    console.log(`driver info ${JSON.stringify(driver, null, 2)}`);
+    expect(driver).toBeDefined();
+  });
+
+  test('update driver phone', async () => {
+    const driverId: string = driverTestData[0].driverId;
+    const updateData = {
+      driverId: driverId,
+      phone: '+6018438920'
+    };
+    const response = await driverManager.update(updateData);
+    console.log(`driver updated ${JSON.stringify(response, null, 2)}`);
+    expect(response).toBeDefined();
+  });
+
+  test.only('delete driver', async () => {
+    let deletedDrivers: Array<EntityModel | Response | undefined> = [];
+    for (const driverInfo of driverTestData) {
+      const driverId: string = driverInfo.driverId;
+      const deleted = await driverManager.delete(driverId);
+      deletedDrivers.push(deleted);
+      console.log(`driver deleted ${JSON.stringify(deleted, null, 2)}`);
+      expect(deleted).toBeDefined();
+    }
+    expect(deletedDrivers).toHaveLength(driverTestData.length);
+    await new Promise((r) => setTimeout(r, 2000));
+  });
+  // required with this small example
 });
-// required with this small example
-// to make the isolatedModules config happy
-export {};
--- src/database/driver.ts
+++ src/database/driver.ts
@@ -1,88 +1,23 @@
-import table from './util';
+import BaseEntity from './base';
 
-export type Driver = {
-  driverId: String;
-  name: String;
-  lastName: String;
-  companyId: String;
-  birthdate: String;
-  phone: String;
-};
-export type DriverUpdate = {
-  driverId: String;
-  name?: String;
-  lastName?: String;
-  companyId?: String;
-  birthdate?: String;
-  phone?: String;
-};
+class Driver extends BaseEntity {
+  constructor(tableName?: string) {
+    super({ tableName, modelName: 'DRIVER', idField: 'driverId' });
+  }
 
-const Driver = table.getModel('Driver');
-
-async function create(driverInfo: Driver) {
-  console.log('Creating new driver in database');
-  try {
-    const newDriver = await Driver.create(driverInfo, { exists: false });
-    return newDriver;
-  } catch (err) {
-    console.log('DynamoDB error: ', err);
-    return null;
+  async listByCompany(companyId) {
+    console.log(`Listing drivers for company ${companyId}`);
+    try {
+      const listByCompany = await this.model.find(
+        { companyId },
+        { index: 'gs1' }
+      );
+      return listByCompany;
+    } catch (error) {
+      console.log('DynamoDB error: ', error);
+      return null;
+    }
   }
 }
 
-async function get(id: String) {
-  console.log('Retrieving driver with id: ', id);
-  try {
-    const driverInfo = await Driver.get({ driverId: id });
-    console.log(driverInfo);
-    return driverInfo;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function list() {
-  console.log('Listing drivers in database');
-  try {
-    const driversList = await Driver.find({}, { index: 'gs1' });
-    return driversList;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function listByCompany(id: String) {
-  console.log('Listing drivers for company ', id);
-  try {
-    const driversList = await Driver.find({ companyId: id }, { index: 'gs1' });
-    return driversList;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function update(data: DriverUpdate) {
-  console.log('Updating driver ', data.driverId);
-  try {
-    const driver = await Driver.update(data);
-    return driver;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function remove(id: String) {
-  console.log('Deleting driver ', id);
-  try {
-    const removed = await Driver.remove({ driverId: id });
-    return removed;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-export { list, listByCompany, get, create, update, remove };
+export { Driver };

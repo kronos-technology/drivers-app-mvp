1c8aaa78866dfc483864e0f6ce9c5906c9560362
Anderson <andersong.9012@gmail.com>
2022-05-23 19:18:25 -0500

    Add tests for company entity

--- src/database/__tests__/company.test.ts
+++ src/database/__tests__/company.test.ts
@@ -1,96 +1,76 @@
-import * as companyLib from '../company';
+import { Company } from '../entities';
+import { EntityModel, CompanyType, Response } from '../base';
 
-test('Create company', async () => {
-  const data: Array<companyLib.Company> = [
-    {
-      companyId: 'SABANA',
-      phone: '+576012635301',
-      address: 'DIAGONAL 23 69 60 OF 702',
-      name: 'Expreso De La Sabana SAS',
-      city: 'Bogota'
-    },
-    {
-      companyId: 'AYACUCHO',
-      phone: '+576014203733',
-      address: 'Carrera 2 # 15 - 31',
-      name: 'Flota Ayacucho LTDA',
-      city: 'Facatativa'
-    },
-    {
-      companyId: 'VILLETAX',
-      phone: '+576018901288',
-      address: 'Carrera 3 # 9 - 35',
-      name: 'Transportes Villetax SA',
-      city: 'Facatativa'
-    }
-  ];
-  for (const item of data) {
-    const response = await companyLib.create(item);
-    expect(response).toBeDefined();
+const companyTestData: Array<CompanyType> = [
+  {
+    companyId: 'SABANA',
+    phone: '+576012635301',
+    address: 'DIAGONAL 23 69 60 OF 702',
+    name: 'Expreso De La Sabana SAS',
+    city: 'Bogota'
+  },
+  {
+    companyId: 'AYACUCHO',
+    phone: '+576014203733',
+    address: 'Carrera 2 # 15 - 31',
+    name: 'Flota Ayacucho LTDA',
+    city: 'Facatativa'
+  },
+  {
+    companyId: 'VILLETAX',
+    phone: '+576018901288',
+    address: 'Carrera 3 # 9 - 35',
+    name: 'Transportes Villetax SA',
+    city: 'Facatativa'
   }
-  await new Promise((r) => setTimeout(r, 2000));
-});
+];
 
-test('List companies', async () => {
-  const response = await companyLib.list();
-  console.log(response);
-  expect(response).toBeInstanceOf(Array);
-});
+describe('company tests', () => {
+  let companyManager: Company;
 
-test('Get company', async () => {
-  const id = 'SABANA';
-  const response = await companyLib.get(id);
-  console.log(response);
-  expect(response).toBeDefined();
-});
+  beforeAll(async () => {
+    companyManager = new Company();
+  });
 
-test('Update company', async () => {
-  const id = 'SABANA';
-  const email = 'operacionesflotaayacucho@gmail.com';
-  const updateData: companyLib.CompanyUpdate = { companyId: id, email: email };
-  const response = await companyLib.update(updateData);
-  console.log(response);
-  expect(response).toBeDefined();
-});
+  test('create test companies', async () => {
+    let createdCompanies: Array<EntityModel | Response> = [];
+    for (const companyInfo of companyTestData) {
+      const created = await companyManager.create(companyInfo);
+      createdCompanies.push(created);
+      console.log(`company created ${JSON.stringify(created, null, 2)}`);
+      expect(created).toBeDefined();
+    }
+    expect(createdCompanies).toHaveLength(companyTestData.length);
+    await new Promise((r) => setTimeout(r, 2000));
+  });
 
-test('Delete company', async () => {
-  const id = 'SABANA';
-  const company = await companyLib.get(id);
-  const removed = await companyLib.remove(id);
-  console.log('company removed: ', removed);
-  expect(removed).toEqual(company);
-});
+  test('list all companies', async () => {
+    const list = await companyManager.list();
+    console.log(`List of all companies: ${JSON.stringify(list, null, 2)}`);
+    expect(list).toBeInstanceOf(Array);
+    expect(list).toHaveLength(companyTestData.length);
+  });
 
-test('Assign route to company', async () => {
-  const companyId = 'AYACUCHO';
-  const routeId = 'FACA-BOGO-13';
-  const addedRoute = await companyLib.assignRoute(companyId, routeId);
-  console.log(
-    `Route ${routeId} assigned to company ${companyId}. ${addedRoute}`
-  );
-  expect(addedRoute).toBeDefined();
-});
+  test('get company', async () => {
+    const companyId: string = companyTestData[0].companyId;
+    const company: EntityModel | undefined = await companyManager.get(
+      companyId
+    );
+    console.log(`Company info ${JSON.stringify(company, null, 2)}`);
+    expect(company).toBeDefined();
+  });
 
-test('Unassign route from company', async () => {
-  const companyId = 'SABANA';
-  const routeId = 'FACA-BOGO-13';
-  const addedRoute = await companyLib.unassignRoute(companyId, routeId);
-  console.log(
-    `Route ${routeId} unassigned from company ${companyId}. ${JSON.stringify(
-      addedRoute
-    )}`
-  );
-  expect(addedRoute).toBeDefined();
+  test('delete company', async () => {
+    let deletedCompanys: Array<EntityModel | Response | undefined> = [];
+    for (const companyInfo of companyTestData) {
+      const companyId: string = companyInfo.companyId;
+      const deleted = await companyManager.delete(companyId);
+      deletedCompanys.push(deleted);
+      console.log(`company deleted ${JSON.stringify(deleted, null, 2)}`);
+      expect(deleted).toBeDefined();
+    }
+    expect(deletedCompanys).toHaveLength(companyTestData.length);
+    await new Promise((r) => setTimeout(r, 2000));
+  });
+  // required with this small example
 });
-
-test('Get assigned routes', async () => {
-  const companyId = 'SABANA';
-  const assignedRoutes = await companyLib.getAssignedRoutes(companyId);
-  console.log(
-    `Routes assigned to company ${companyId}: ${JSON.stringify(assignedRoutes)}`
-  );
-  expect(assignedRoutes).toBeDefined();
-});
-// required with this small example
-// to make the isolatedModules config happy
-export {};
--- src/database/company.ts
+++ src/database/company.ts
@@ -1,127 +1,50 @@
-import table from './util';
+import BaseEntity, { EntityModel } from './base';
+import { Model } from 'dynamodb-onetable';
 
-export type Company = {
-  companyId: String;
-  name: String;
-  phone: String;
-  address?: String;
-  email?: String;
-  city: String;
-};
+class Company extends BaseEntity {
+  routeModel: Model<EntityModel>;
+  constructor(tableName?: string) {
+    super({ tableName, modelName: 'COMPANY', idField: 'companyId' });
+    this.routeModel = this.table.getModel<EntityModel>('ROUTE');
+  }
 
-export type CompanyUpdate = {
-  companyId: String;
-  name?: String;
-  phone?: String;
-  address?: String;
-  email?: String;
-  city?: String;
-};
-const CompanyModel = table.getModel('Company');
-const RouteInCompany = table.getModel('RouteInCompany');
+  async assignRoute(companyId: string, routeId: string) {
+    console.log(`Assigning route ${routeId} to company ${companyId}`);
+    try {
+      const assignedRoute = await this.routeModel.create({
+        companyId,
+        routeId
+      });
+      return assignedRoute;
+    } catch (error) {
+      console.log('DynamoDB error: ', error);
+      return null;
+    }
+  }
 
-async function create(companyInfo: Company) {
-  console.log('Creating new company in database');
-  try {
-    const newCompany = await CompanyModel.create(companyInfo, {
-      exists: false
-    });
-    return newCompany;
-  } catch (err) {
-    console.log('DynamoDB error: ', err);
-    return null;
+  async unassignRoute(companyId: string, routeId: string) {
+    console.log(`Unassigning route ${routeId} from company ${companyId}`);
+    try {
+      const unassignedRoute = await this.routeModel.remove({
+        companyId: companyId,
+        routeId: routeId
+      });
+      return unassignedRoute;
+    } catch (error) {
+      console.log('DynamoDB error: ', error);
+      return null;
+    }
+  }
+
+  async getAssignedRoutes(companyId: string) {
+    console.log(`Listing routes assigned to company ${companyId}`);
+    try {
+      const assignedRoutes = await this.routeModel.find({ companyId });
+      return assignedRoutes;
+    } catch (error) {
+      console.log('DynamoDB error: ', error);
+      return null;
+    }
   }
 }
-
-async function get(id: String) {
-  console.log('Retrieving company with id: ', id);
-  try {
-    const companyInfo = await CompanyModel.get({ companyId: id });
-    console.log(companyInfo);
-    return companyInfo;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function list() {
-  console.log('Listing companies in database');
-  try {
-    const companiesList = await CompanyModel.find({}, { index: 'gs1' });
-    return companiesList;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function update(data: CompanyUpdate) {
-  console.log('Updating company ', data.companyId);
-  try {
-    const company = await CompanyModel.update(data);
-    return company;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function remove(id: String) {
-  console.log('Deleting company ', id);
-  try {
-    const removed = await CompanyModel.remove({ companyId: id });
-    return removed;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function assignRoute(companyId: String, routeId: String) {
-  try {
-    const addedRoute = await RouteInCompany.create({
-      companyId: companyId,
-      routeId: routeId
-    });
-    return addedRoute;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function unassignRoute(companyId: String, routeId: String) {
-  console.log(`Deleting route ${routeId} from company ${companyId}`);
-  try {
-    const removedRoute = await RouteInCompany.remove({
-      companyId: companyId,
-      routeId: routeId
-    });
-    return removedRoute;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-
-async function getAssignedRoutes(companyId: String) {
-  console.log(`Retrieving routes assigned to company ${companyId}`);
-  try {
-    const assignedRoutes = await RouteInCompany.find({ companyId });
-    return assignedRoutes;
-  } catch (error) {
-    console.log('DynamoDB error: ', error);
-    return null;
-  }
-}
-export {
-  list,
-  get,
-  create,
-  update,
-  remove,
-  assignRoute,
-  getAssignedRoutes,
-  unassignRoute
-};
+export { Company };

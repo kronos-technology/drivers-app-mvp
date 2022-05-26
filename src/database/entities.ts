9b9544f35ce4c00885ff0c217c40e84a4fbc9f02
Anderson <andersong.9012@gmail.com>
2022-05-25 20:02:56 -0500

    Deletes entities.ts

--- src/database/entities.ts
+++
@@ -1,6 +0,0 @@
-export { Vehicle } from './vehicle';
-export { Route } from './route';
-export { Driver } from './driver';
-export { Company } from './company';
-export { Checkpoint } from './checkpoint';
-export { Checkin } from './checkin';

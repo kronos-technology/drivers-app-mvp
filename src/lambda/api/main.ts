5cbec16ab8c393fa686fd14b0ae8cfa10508c312
Anderson <andersong.9012@gmail.com>
2022-05-25 20:03:11 -0500

    import database adapter in api lambda

--- src/lambda/api/main.ts
+++ src/lambda/api/main.ts
@@ -1,3 +1,12 @@
+import {
+  Checkin,
+  Checkpoint,
+  Company,
+  Driver,
+  Route,
+  Vehicle
+} from '../../database/adapter';
+
 export const handler = async (event) => {
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
   return {
